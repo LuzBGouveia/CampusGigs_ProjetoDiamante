1f54dd93aa5bcda35164e5064e97ee9bb1e10c77
LuzBGouveia <lu.barros.gouveia2@gmail.com>
2026-09-16 10:43:32 -0300

    feat: Adicionando service do Servico.

--- src/main/java/fiap/com/br/campusgigs/servico/ServicoController.java
+++ src/main/java/fiap/com/br/campusgigs/servico/ServicoController.java
@@ -1,7 +1,29 @@
 package fiap.com.br.campusgigs.servico;
 
+import fiap.com.br.campusgigs.servico.Servico;
+import fiap.com.br.campusgigs.servico.ServicoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @RestController
+@RequestMapping("/servico")
 public class ServicoController {
+    private final ServicoService service;
+
+    @GetMapping
+    public List<Servico> findAll() {
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Servico> findById(@PathVariable Long id) {
+        return ResponseEntity.of(service.findById(id));
+    }
 }
--- src/main/java/fiap/com/br/campusgigs/servico/ServicoService.java
+++ src/main/java/fiap/com/br/campusgigs/servico/ServicoService.java
@@ -1,7 +1,21 @@
 package fiap.com.br.campusgigs.servico;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class ServicoService {
+    private final ServicoRepository repository;
+
+    public List<Servico> findAll() {
+        return repository.findAll();
+    }
+
+    public Optional<Servico> findById(Long id) {
+        return repository.findById(id);
+    }
 }
